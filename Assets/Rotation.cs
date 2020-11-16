4ee4eb207b5fb17a585dfe3ab5ae419f3b270b4a
Alex Dimitrijevic <baiomurang@gmail.com>
2020-11-15 23:32:28 -0600

    Planet with more vertices, rotation is handled outside of shader, No tileable noise yet :(
    Tileable noise still eludes me. Busy week so I didn't have a lot of time to look up how to make my own. Next week.

---
+++ Assets/Rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float rotationSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + rotationSpeed * Time.deltaTime, transform.rotation.eulerAngles.z);
+    }
+}
